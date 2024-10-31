dd6469cadb7e0c7bc0c3575ff4405f83c16b6a8d
John Rhendell B. Ong <jrtist.2004@gmail.com>
2024-10-31 13:53:37 +0800

    added a pic and css design

--- Static Login/index.php
+++ Static Login/index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Static Login</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -18,6 +19,7 @@
 
     <div class="alert alert-success" role="alert" id="alert_success_custom" name="alrt_success" style="display: none;">
         <button type="button" class="btn-close" aria-label="Close" id="close_success"></button>
+        Welcome to the System: <?php $email = $_POST['floatingInput'] ?? ''; echo "$email"; ?>
     </div>
 
     <div class="round-container text-center" id="cntnr">
